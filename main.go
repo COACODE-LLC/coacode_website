bd28bcef092c5541993ba20803c319537e32c0ad
naddiemathkour <naddiemathkour@gmail.com>
2024-02-05 19:19:41 -0500

    hello world template server

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
   "fmt"
+  "text/template"
+  "net/http"
+  "log"
 )
 
 func main() {
   //server code here
   fmt.Println("Hello World")
+
+  h1 := func(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("./html/index.html"))
+    tmpl.Execute(w, nil)
+  }
+
+  http.HandleFunc("/", h1)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
