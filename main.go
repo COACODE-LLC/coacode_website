7ff5e21017222c66df0c655ea3f3d1e92e9531e1
naddiemathkour <naddiemathkour@gmail.com>
2024-02-06 20:52:54 -0500

    added contact page and mux connections

--- main.go
+++ main.go
@@ -5,41 +5,42 @@ import (
   "text/template"
   "net/http"
   "log"
+  "time"
 )
-
-type PageData struct {
-  Title string
-}
 
 func main() {
   //server code here
-  fmt.Println("Hello World")
-
-  homeHandler := func(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("./html/index.html"))
-    tmpl.Execute(w, nil)
-    log.Println(r)
-  }
+  fmt.Println("Initializing http server...")
 
   http.HandleFunc("/", homeHandler)
   http.HandleFunc("/about", aboutHandler)
+  http.HandleFunc("/contact", contactHandler)
+
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func printLogs() {
+  time.Sleep(1000 * time.Millisecond)
+  fmt.Println("Server online: Localhost:8080")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+  log.Println("Hello Contact")
+  renderTemplate(w, "html/contact.html")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+  log.Println("Hello Home")
+  renderTemplate(w, "html/index.html")
+}
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
   log.Println("Hello About")
-
   renderTemplate(w, "html/about.html")
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-  //Parse HTML template files
-  /*tmplFiles := []string {
-    "html/index.html",
-    "html/about.html",
-  }*/
-
+  //Parse template file
   t, err := template.ParseFiles(tmpl);
   check(err, w)
 
