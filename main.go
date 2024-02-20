1d7ab66d66ded03dbbcdcf19029fdfe5c5939367
naddiemathkour <naddiemathkour@gmail.com>
2024-02-20 16:38:51 -0500

    cleaned main. Added favicon

--- main.go
+++ main.go
@@ -13,38 +13,31 @@ func main() {
   fmt.Println("Initializing http server to...")
   fmt.Println("Connected to:", termlink.Link("localhost:8080", "http://localhost:8080")) 
 
- // http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("src/assets"))))
+  //handle html template serving
+  http.HandleFunc("/", pageHandler)
 
-  // Serve static files from the "./src/assets" directory
-    http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "./src/assets/"+r.URL.Path[8:])
-    })
-  
-  //create defualtmux connections
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/about", aboutHandler)
-  http.HandleFunc("/contact", contactHandler)
+  //handle static file serving
+  http.HandleFunc("/assets/", assetHandler)
   
   //Create server on port :8080
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-  log.Println("Hello Home")
-  renderTemplate(w, "index.html")
+func assetHandler(w http.ResponseWriter, r *http.Request) {
+  //gets url path from r, searches for file in src/assets/
+  http.ServeFile(w, r, "./src/assets/"+r.URL.Path[8:])
 }
 
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-  log.Println("Hello About")
-  renderTemplate(w, "src/pages/about.html")
-}
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+  pages := map[string]string {
+    "/": "index.html",
+    "/about": "src/pages/about.html",
+    "/contact": "src/pages/contact.html",
+  }
 
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-  log.Println("Hello Contact")
-  renderTemplate(w, "src/pages/contact.html")
+  renderTemplate(w, pages[r.URL.Path])
 }
 
 
