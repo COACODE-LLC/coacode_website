eec8b7316706a9423bf06a9fc4a98a243e299244
naddiemathkour <naddiemathkour@gmail.com>
2024-02-23 11:14:33 -0500

    routing improvements (again)

--- main.go
+++ main.go
@@ -1,65 +1,93 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 
 	"github.com/gorilla/mux"
 	"github.com/savioxavier/termlink"
 )
 
+var pageTemplates = map[string]string {
+  "/":        "index.html",
+  "/about":   "src/pages/about.html",
+  "/contact": "src/pages/contact.html",
+  
+}
+
 func main() {
-	//server code here
-	fmt.Println("Initializing http server to...")
-	fmt.Println("Connected to:", termlink.Link("localhost:8080", "http://localhost:8080"))
+  //server code here
+  log.Println("Initializing http server to...")
+  log.Println("Connected to:", termlink.Link("localhost:8080", "http://localhost:8080"))
 
-	//create Gorilla router
-	router := mux.NewRouter()
+  //create Gorilla router
+  router := mux.NewRouter()
 
-	//handle html template serving
-	router.HandleFunc("/", homeHandler)
-	router.HandleFunc("/{path}", pageHandler)
+  //handle html template serving
+  router.HandleFunc("/", homeHandler)
+  router.HandleFunc("/{page}", pageHandler)
 
-	//handle asset serving
-	router.HandleFunc("/assets/{file}", assetHandler)
+  //handle asset serving
+  router.HandleFunc("/assets/{file}", assetHandler)
 
-	//Create server on port :8080
-	log.Fatal(http.ListenAndServe(":8080", router))
+  //Create server on port :8080
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("home", r.URL.Path)
-	renderTemplate(w, "index.html")
+  log.Println("home", r.URL.Path)
+  renderTemplate(w, "index.html")
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("page", r.URL.Path)
-	if r.URL.Path == "/favicon.ico" {
-		return
-	}
-	tmpl := "src/pages" + r.URL.Path + ".html"
+  log.Println("page", r.URL.Path)
+  path := r.URL.Path
+  if path == "/favicon.ico" { 
+    http.ServeFile(w, r, path)
+    return
+  }
 
-	renderTemplate(w, tmpl)
+  tmpl, ok := pageTemplates[path]
+  if !ok {
+    http.NotFound(w, r)
+    return
+  }
+
+  renderTemplate(w, tmpl)
 }
 
 func assetHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/assets/"+r.URL.Path[8:])
+  path := "src/assets/" + r.URL.Path[len("/assets/"):]
+  contentType := getContentType(path)
+  w.Header().Set("Content-Type", contentType)
+  http.ServeFile(w, r, path)
+}
+
+func getContentType(path string) string {
+  switch filepath.Ext(path) {
+  case ".css":              return "text/css"
+  case ".jpg", ".jpeg":     return "image/jpeg"
+  case ".png":              return "image/png"
+  case ".ico":              return "image/x-icon"
+  case ".gif":              return "image/gif"
+  default:                  return "application/octet-stream"
+  }
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	//Parse template file
-	t, err := template.ParseFiles(tmpl)
-	check(err, w)
-
-	//Execute Template
-	err = t.Execute(w, nil)
-	check(err, w)
+  //Parse template file
+  t, err := template.ParseFiles(tmpl)
+  check(err, w)
+  //Execute Template
+  err = t.Execute(w, nil)
+  check(err, w)
 }
 
 func check(err error, w http.ResponseWriter) {
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
+
